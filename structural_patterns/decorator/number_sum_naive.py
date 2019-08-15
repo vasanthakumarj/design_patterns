06da059db3f8005a8031be787d55582ccacc0db3
jvasa <jvasanthirtt05@gmail.com>
2019-08-15 19:53:41 +0530

    decorator-pattern

--- structural_patterns/decorator/number_sum_naive.py
+++ structural_patterns/decorator/number_sum_naive.py
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     from timeit import Timer
     #t = Timer('memo_number_sum(30)', 'from __main__ import memo_number_sum') ##Time:  0.18827976000000002
     t = Timer('memo_number_sum(300)', 'from __main__ import memo_number_sum') ##Time:  0.181029102
-    print('Time: ', t.timeit())
+    print('Time: ', t.timeit()) 
